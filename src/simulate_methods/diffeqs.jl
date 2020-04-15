c90bd94aced851faee64a2371c7e986ca13283b0
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-04-14 22:49:55 -0400

    make the eps time dependent for safety

--- src/simulate_methods/diffeqs.jl
+++ src/simulate_methods/diffeqs.jl
@@ -125,7 +125,7 @@ function ith_subject_cb(pre,datai::Subject,u0,t0,ProbType,saveat,save_discont,co
       f = integrator.f
     end
 
-    while counter <= length(events) && events[counter].time <= integrator.t+sqrt(eps())
+    while counter <= length(events) && events[counter].time <= integrator.t+sqrt(eps(t))
       cur_ev = events[counter]
       @inbounds if (cur_ev.evid == 1 || cur_ev.evid == -1) && cur_ev.ss == 0
         dose!(integrator,integrator.u,cur_ev,last_restart)
